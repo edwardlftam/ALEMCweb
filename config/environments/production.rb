d3d5a03f131fb0baf9cc1155ea9d5752b492df0b
edward6882990 <edward6882990@gmail.com>
2012-11-21 03:42:05 -0500

    fixing environment for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ ALEMC::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-   config.assets.precompile += [ custom.css ]
+  # config.assets.precompile += [ custom.css ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
